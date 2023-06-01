8defd6cb5278b79bf0a63b4117ffddfeea5ee528
johnkdunyo <johnkdunyo@gmail.com>
2023-06-01 11:33:37 +0000

    added site metadata

---
+++ src/lib/siteMetaData.ts
@@ -0,0 +1,22 @@
+const siteMetadata = {
+  title: 'Leash Pay',
+  author: 'Jon Dexter',
+  description:
+    "Official website for leashPay",
+  language: 'en-us',
+  siteUrl: '',
+  siteRepo: 'https://github.com/johnkdunyo/',
+  email: 'mailto:mail@gmail.com',
+  github: '',
+  twitter: '',
+  linkedin: '',
+  newsletter: '',
+  snippets:
+    'https://www.notion.so/9f35162787e0448094188b66304cf9f6?v=1b9c4e146edf46148fe7b1c5346cbea4',
+  locale: 'en-US',
+  analytics: {
+    plausibleDataDomain: 'johnkdunyo.dev'
+  }
+};
+
+export default siteMetadata
