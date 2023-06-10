f8e4bcba5c3cd96fd43b7451191ea9231a7d394a
johnkdunyo <johnkdunyo@gmail.com>
2023-06-10 13:15:39 +0000

    fixed breaking bugs

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -30,7 +30,7 @@ const CustomLinkButton = ({ text, href = "/" }: CustomLinkButtonProps) => {
   );
 };
 
-const index = () => {
+const Index = () => {
   const [openMobileMenu, setOpenMobileMenu] = useState(false);
   const mobileMenuRef = useRef<HTMLDivElement>(null);
 
@@ -172,4 +172,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Index;
