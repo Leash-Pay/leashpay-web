2fa652a030ef7a15fbc40245a9d2a4842a880087
johnkdunyo <johnkdunyo@gmail.com>
2023-06-13 09:55:59 +0000

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import "./globals.css";
 import { Encode_Sans } from "next/font/google";
 import siteMetadata from "@/lib/siteMetaData";
+import { Metadata } from "next";
 
 const encodeSans = Encode_Sans({
   subsets: ["latin"],
   variable: "--font-encode",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: siteMetadata.title,
   description: siteMetadata.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
   generator: "Next.js",
   applicationName: siteMetadata.title,
   referrer: "origin-when-cross-origin",
